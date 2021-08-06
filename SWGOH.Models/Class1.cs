181f516ad307e1b888950a2b8d0be17f7be5af21
Johnson <Johnson>
2021-08-06 21:44:52 +0800

    init

---
+++ SWGOH.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SWGOH.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ SWGOH.Tools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SWGOH.Tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
